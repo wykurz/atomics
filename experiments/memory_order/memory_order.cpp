83b81130722f80f36f4e0255d015a6d3d242702e
Mateusz Wykurz <wykurz@gmail.com>
2015-11-01 19:25:58 -0500

    Progressing on the experimental path...

--- experiments/memory_order/memory_order.cpp
+++ experiments/memory_order/memory_order.cpp
@@ -5,28 +5,56 @@
 #include <cstdint>
 #include <benchmark/benchmark.h>
 
-constexpr std::size_t SIZE = 1024;
+constexpr std::size_t SIZE = 1 << 10;
 
-template<std::memory_order LoadOrder, std::memory_order StoreOrder>
-void loadStore(benchmark::State& state_)
+template<typename Type,
+         std::size_t Size,
+         std::memory_order LoadOrder,
+         std::memory_order StoreOrder>
+struct StoreLoadFixture: public ::benchmark::Fixture
 {
-    std::array<std::atomic<std::int64_t>, SIZE> array;
-    while (state_.KeepRunning()) {
-        state_.PauseTiming();
+    void SetUp()
+    {
+        // static assert Size is power of 2
         std::fill(array.begin(), array.end(), 0);
-        state_.ResumeTiming();
-        for (int i = 0; i < state_.range_x(); ++i)
+    }
+
+    void store(std::size_t index_, Type value_)
+    {
+        array[index_ & Size].store(value_, StoreOrder);
+    }
+
+    Type load(std::size_t index_)
+    {
+        return array[index_ & Size].load(LoadOrder);
+    }
+
+  private:
+    std::array<std::atomic<Type>, SIZE> array;
+};
+
+using AcquireReleaseFixture = StoreLoadFixture<std::int64_t,
+                                               SIZE,
+                                               std::memory_order_acquire,
+                                               std::memory_order_release>;
+
+BENCHMARK_DEFINE_F(AcquireReleaseFixture, LoadStoreTest)(benchmark::State& state_)
+{
+    if (state_.thread_index == 0) {
+        while (state_.KeepRunning())
+            for (int i = 0; i < state_.range_x(); ++i)
+                store(i, i);
+    }
+    else {
+        while (state_.KeepRunning())
         {
-            array[i].store(i, StoreOrder);
-        }
-        std::int64_t sum = 0;
-        for (int i = 0; i < state_.range_x(); ++i)
-        {
-            sum += array[i].load(LoadOrder);
+            std::int64_t sum = 0;
+            for (int i = 0; i < state_.range_x(); ++i)
+                sum += load(i);
         }
     }
 }
 
-BENCHMARK_TEMPLATE(loadStore, std::memory_order_acquire, std::memory_order_release)->Range(1, SIZE);
+BENCHMARK_REGISTER_F(AcquireReleaseFixture, LoadStoreTest)->Threads(2)->Range(1 << 10, 1 << 20);
 
 BENCHMARK_MAIN();
