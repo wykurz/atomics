b045862967fe70e93caa87fe6646367855e22410
Mateusz Wykurz <wykurz@gmail.com>
2015-11-01 19:01:03 -0500

    Try to test something semi-useful (unsucessfully ;P)

--- experiments/memory_order/memory_order.cpp
+++ experiments/memory_order/memory_order.cpp
@@ -1,40 +1,32 @@
-#include <benchmark/benchmark.h>
+#include <algorithm>
+#include <array>
+#include <atomic>
 #include <cassert>
+#include <cstdint>
+#include <benchmark/benchmark.h>
 
-class MyFixture : public ::benchmark::Fixture
+constexpr std::size_t SIZE = 1024;
+
+template<std::memory_order LoadOrder, std::memory_order StoreOrder>
+void loadStore(benchmark::State& state_)
 {
-  public:
-    void SetUp() {
-        data = new int(42);
-    }
-
-    void TearDown() {
-        assert(data != nullptr);
-        delete data;
-        data = nullptr;
-    }
-
-    ~MyFixture() {
-        assert(data == nullptr);
-    }
-
-    int* data;
-};
-
-
-BENCHMARK_F(MyFixture, Foo)(benchmark::State& st) {
-    assert(data != nullptr);
-    assert(*data == 42);
-    while (st.KeepRunning()) {
+    std::array<std::atomic<std::int64_t>, SIZE> array;
+    while (state_.KeepRunning()) {
+        state_.PauseTiming();
+        std::fill(array.begin(), array.end(), 0);
+        state_.ResumeTiming();
+        for (int i = 0; i < state_.range_x(); ++i)
+        {
+            array[i].store(i, StoreOrder);
+        }
+        std::int64_t sum = 0;
+        for (int i = 0; i < state_.range_x(); ++i)
+        {
+            sum += array[i].load(LoadOrder);
+        }
     }
 }
 
-BENCHMARK_DEFINE_F(MyFixture, Bar)(benchmark::State& st) {
-    while (st.KeepRunning()) {
-    }
-    st.SetItemsProcessed(st.range_x());
-}
-BENCHMARK_REGISTER_F(MyFixture, Bar)->Arg(42);
+BENCHMARK_TEMPLATE(loadStore, std::memory_order_acquire, std::memory_order_release)->Range(1, SIZE);
 
-
-BENCHMARK_MAIN()
+BENCHMARK_MAIN();
