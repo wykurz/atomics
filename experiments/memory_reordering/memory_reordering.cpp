81597d17f985bbd1e0d2088813d038bc5b7cfe25
Mateusz Wykurz <wykurz@gmail.com>
2015-11-07 16:18:02 -0500

    Some code cleanup, still no success in reproducing reordering on my x86 PC but hey, the code is neat.

--- experiments/memory_reordering/memory_reordering.cpp
+++ experiments/memory_reordering/memory_reordering.cpp
@@ -10,38 +10,40 @@
 
 constexpr std::size_t NUM_THREADS = 2;
 
-using TestType = int;
+using TestType = std::size_t;
 
-static std::array<TestType, NUM_THREADS> mem;
-static std::array<TestType, NUM_THREADS> ret;
+static std::array<std::atomic<TestType>, NUM_THREADS> mem;
+static std::array<std::atomic<TestType>, NUM_THREADS> ret;
 
+template<std::size_t ThreadId,
+         std::memory_order LoadOrder,
+         std::memory_order StoreOrder>
 struct StoreLoad
 {
-    StoreLoad(std::size_t thread_)
-        : thread(thread_)
+    StoreLoad()
     {
-        static_assert(2 == NUM_THREADS, "StoreLoad test requires exactly 2 threads.");
-        assert(thread < NUM_THREADS);
+        static_assert(2 == NUM_THREADS,
+                      "StoreLoad test requires exactly 2 threads.");
+        static_assert(ThreadId < NUM_THREADS,
+                      "ThreadId must be less than total threads.");
     }
 
     void run()
     {
-        TestType& x = mem[thread];
-        TestType& y = mem[(thread + 1) & NUM_THREADS];
         // Reordering means both functions return 0
-        TestType r = 0;
         delay();
-        x = 1;
+        mem[x].store(1, StoreOrder);
         asm volatile("" ::: "memory");
-        r = y;
-        ret[thread] = r;
+        const auto r = mem[y].load(LoadOrder);
+        ret[x].store(r, StoreOrder);
     }
 
     void check()
     {
         if (not isControl())
             return;
-        reordered += (0 == std::accumulate(ret.begin(), ret.end(), 0)) ? 1 : 0;
+        if (ret[x] == 0 and ret[y] == 0)
+            ++reordered;
         ++checks;
     }
 
@@ -50,55 +52,60 @@ struct StoreLoad
         if (not isControl())
             return;
         std::fill(mem.begin(), mem.end(), 0);
-        std::fill(ret.begin(), ret.end(), 0);
     }
 
     void print() const
     {
         if (not isControl())
             return;
-        std::cerr << "Reordered: " << reordered << std::endl;
-        std::cerr << "Checks:    " << checks << std::endl;
+        std::cerr << "Reordered:   " << reordered << std::endl;
+        std::cerr << "Checks:      " << checks << std::endl;
+        std::cerr << "Wait cycles: " << waitCycles << std::endl;
     }
 
   private:
     void delay()
     {
         constexpr std::size_t maxSpins = 8;
-        std::size_t spins = ((thread + 2) * checks) % maxSpins;
-        while (0 < spins--) {};
+        std::size_t spins = ((ThreadId + 2) * checks) % maxSpins;
+        while (0 < spins--) { ++waitCycles; };
     }
 
     bool isControl() const
     {
-        return 0 == thread;
+        return 0 == ThreadId;
     }
 
-    const std::size_t thread;
+    static constexpr auto x = ThreadId;
+    static constexpr auto y = (ThreadId + 1) & NUM_THREADS;
+
     std::size_t reordered = 0;
     std::size_t checks = 0;
+    std::size_t waitCycles = 0;
 };
 
 static std::atomic<bool> done(false);
 
-void synchronize(const std::size_t round)
+bool synchronize(const std::size_t round)
 {
     static std::atomic<std::size_t> sync(0);
     ++sync;
     while (not done and sync < round * NUM_THREADS) {}
+    return done;
 }
 
 template<typename Test>
-void run(Test& test)
+void run()
 {
+    Test test;
     std::size_t round = 0;
     while (not done)
     {
-        synchronize(++round);
+        if (synchronize(++round)) break;
         test.reset();
-        synchronize(++round);
+        if (synchronize(++round)) break;
         test.run();
-        synchronize(++round);
+        if (synchronize(++round)) break;
         test.check();
     }
     test.print();
@@ -106,11 +113,11 @@ void run(Test& test)
 
 int main()
 {
-    StoreLoad sl0(0);
-    StoreLoad sl1(1);
-    std::thread t1([&](){ run(sl0); });
-    std::thread t2([&](){ run(sl1); });
-    std::this_thread::sleep_for(std::chrono::seconds(10));
+    constexpr auto loadOrder = std::memory_order_relaxed;
+    constexpr auto storeOrder = std::memory_order_relaxed;
+    std::thread t1(run<StoreLoad<0, loadOrder, storeOrder>>);
+    std::thread t2(run<StoreLoad<1, loadOrder, storeOrder>>);
+    std::this_thread::sleep_for(std::chrono::seconds(3));
     done = true;
     t1.join();
     t2.join();
--- wscript
+++ wscript
@@ -7,7 +7,7 @@ def options(opt):
 def configure(conf):
     conf.find_program('clang++-3.6', var='CXX', mandatory=True)
     conf.load('compiler_cxx')
-    conf.env.CXXFLAGS = ['-std=c++11', '-O2', '-g', '-Wall']
+    conf.env.CXXFLAGS = ['-std=c++11', '-O3', '-g', '-Wall']
 
 def build(bld):
     bld.program(source='experiments/memory_order/memory_order.cpp',
